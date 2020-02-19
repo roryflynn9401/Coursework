db2f74a3bfb5ef5d26ee8fda884fbdecc7ebe1a2
roryflynn9401 <61230733+roryflynn9401@users.noreply.github.com>
2020-02-19 10:50:25 +0000

    Coursework

---
+++ BankAccount.java
@@ -0,0 +1,29 @@
+package BankingSystem;
+
+public class BankAccount {
+	private int accountNumber;
+	private double balance;
+	private static int nextAccNumber = 10000000;
+
+	public BankAccount(AccountType accType, double balance) {
+		this.accountNumber = nextAccNumber;
+		nextAccNumber++;
+		this.balance = balance;
+		this.accType = accType;
+	}
+	public int getAccountNumber() {
+		return this.accountNumber;
+	}
+	public AccountType getAccType() {
+		return this.accType;
+	}
+	public double getBalance() {
+		return this.balance;
+	}
+	public void setAccType(AccountType accType) {
+		this.accType = accType;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
